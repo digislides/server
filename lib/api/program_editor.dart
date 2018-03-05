1c23b108f16be6c1c9a416347cd2053d20aa13f0
tejainece <tejainece@gmail.com>
2018-03-05 16:34:51 +0100

    Work on program edit and getAll

--- lib/api/program_editor.dart
+++ lib/api/program_editor.dart
@@ -69,8 +69,27 @@ class ProgramAccessor {
   Future<Map> get(String id) =>
       db.collection('p').findOne(where.id(new ObjectId.fromHexString(id)));
 
-  Future<Map> update(String id, Map data) =>
+  Future<List<Map>> getByUser(String user) => db
+      .collection('p')
+      .find(where
+        ..or(where.eq('owner', user))
+        ..or(where.oneFrom('writers', [user]))
+        ..or(where.oneFrom('readers', [user])))
+      .toList();
+
+  Future<Map> save(String id, Map data) =>
       db.collection('p').update(where.id(new ObjectId.fromHexString(id)), data);
+
+  Future<Map> edit(String id, String name, int width, int height,
+          List<String> writers, List<String> readers) =>
+      db.collection('p').update(
+          where.id(new ObjectId.fromHexString(id)),
+          modify
+            ..set('name', name)
+            ..set('width', width)
+            ..set('height', height)
+            ..set('writers', writers)
+            ..set('readers', readers));
 
   Future delete(String id) async {
     await db.collection('p').remove(where.id(new ObjectId.fromHexString(id)));
@@ -85,6 +104,13 @@ MongoDb mongo(Context ctx) =>
 class ProgramEditorRoutes {
   String getUserId(Context ctx) => '1' * 24;
 
+  programByIdNotFound(String id) => new Exception();
+
+  doNotHaveReadAccess(String id) => new Exception();
+
+  doNotHaveWriteAccess(String id) => new Exception();
+
+  /// Route to create a new program
   @PostJson()
   Future<Map> create(Context ctx) async {
     String userId = getUserId(ctx);
@@ -97,64 +123,158 @@ class ProgramEditorRoutes {
     return accessor.get(progId.toHexString());
   }
 
+  /// Route to save a program
   @PutJson(path: '/:id')
-  Future<Map> update(Context ctx) async {
+  Future<Map> save(Context ctx) async {
     String userId = getUserId(ctx);
     String progId = ctx.pathParams['id'];
-    // TODO lock program
     Db db = ctx.getInterceptorResult<Db>(MongoDb);
     final accessor = new ProgramAccessor(db);
     Map map = await accessor.get(progId);
-    if (map == null) {
-      throw new Exception(); // TODO
-    }
+    if (map == null) throw programByIdNotFound(progId);
     Program pg = new Program.fromMap(map);
-    if (!pg.hasWriteAccess(userId)) {
-      throw new Exception(
-          'Does not have write access'); // TODO throw appropriate error
-    }
+    if (!pg.hasWriteAccess(userId)) throw doNotHaveWriteAccess(progId);
     Map data = await ctx.req.bodyAsJsonMap();
     data['name'] = map['name'];
     data['owner'] = map['owner'];
     data['writers'] = map['writers'];
     data['readers'] = map['readers'];
     data.remove('_id');
-    await accessor.update(progId, data);
-    // TODO unlock program
+    await accessor.save(progId, data);
     return accessor.get(progId);
   }
 
+  /// Route to get a program by id
   @GetJson(path: '/:id')
   Future<Map> getById(Context ctx) async {
     String userId = getUserId(ctx);
+    String progId = ctx.pathParams['id'];
     Db db = ctx.getInterceptorResult<Db>(MongoDb);
-    Map map = await new ProgramAccessor(db).get(ctx.pathParams['id']);
-    if (map == null) {
-      throw new Exception(); // TODO
-    }
+    Map map = await new ProgramAccessor(db).get(progId);
+    if (map == null) throw programByIdNotFound(progId);
     Program pg = new Program.fromMap(map);
-    if (!pg.hasReadAccess(userId)) {
-      throw new Exception(
-          'Does not have read access'); // TODO throw appropriate error
-    }
+    if (!pg.hasReadAccess(userId))  throw doNotHaveReadAccess(progId);
     return map;
   }
 
+  /// Route to delete a program by id
   @DeleteJson(path: '/:id')
-  delete(Context ctx) async {
+  Future<List<Map>> delete(Context ctx) async {
     String userId = getUserId(ctx);
     String progId = ctx.pathParams['id'];
     Db db = ctx.getInterceptorResult<Db>(MongoDb);
     final accessor = new ProgramAccessor(db);
     Map map = await accessor.get(progId);
-    if (map == null) {
-      throw new Exception(); // TODO
-    }
+    if (map == null) throw programByIdNotFound(progId);
     Program pg = new Program.fromMap(map);
-    if (!pg.hasWriteAccess(userId)) {
-      throw new Exception(
-          'Does not have write access'); // TODO throw appropriate error
-    }
+    if (!pg.hasWriteAccess(userId))  throw doNotHaveWriteAccess(progId);
     await accessor.delete(progId);
+    return accessor.getByUser(userId);
+  }
+
+  @GetJson()
+  Future<List<Map>> getAll(Context ctx) async {
+    String userId = getUserId(ctx);
+    Db db = ctx.getInterceptorResult<Db>(MongoDb);
+    final accessor = new ProgramAccessor(db);
+    return accessor.getByUser(userId);
+  }
+
+  /// Route to duplicate a program
+  Future<List<Map>> duplicate(Context ctx) {
+    // TODO
+  }
+
+  /// Route to edit a program
+  @PutJson(path: '/edit/:id')
+  Future<Map> edit(Context ctx) async {
+    String userId = getUserId(ctx);
+    String progId = ctx.pathParams['id'];
+    Db db = ctx.getInterceptorResult<Db>(MongoDb);
+    final accessor = new ProgramAccessor(db);
+    Map map = await accessor.get(progId);
+    if (map == null) throw programByIdNotFound(progId);
+    Program pg = new Program.fromMap(map);
+    if (!pg.hasWriteAccess(userId))  throw doNotHaveWriteAccess(progId);
+    Map data = await ctx.req.bodyAsJsonMap();
+    await accessor.edit(progId, data['name'], data['width'], data['height'],
+        data['writers'], data['readers']);
+    return accessor.get(progId);
+  }
+}
+
+class Published {
+  String id;
+
+  String name;
+
+  String programId;
+
+  String version;
+
+  dynamic data;
+}
+
+class PublishedRoutes {
+  publish(Context ctx) {
+    // TODO
+  }
+
+  listPublished(Context ctx) {
+    // TODO
+  }
+}
+
+class Frame {
+  String id;
+
+  int width;
+
+  int height;
+
+  String programId;
+
+  String programVersion;
+}
+
+class Player {
+  String id;
+
+  String owner;
+
+  int width;
+
+  int height;
+
+  List<Frame> frames;
+}
+
+class PlayerRoutes {
+  create(Context ctx) {
+    // TODO
+  }
+
+  save(Context ctx) {
+    // TODO
+  }
+
+  delete(Context ctx) {
+    // TODO
+  }
+
+  get(Context ctx) {
+    // TODO
+  }
+
+  getAll(Context ctx) {
+    // TODO
+  }
+
+  duplicate(Context ctx) {
+    // TODO
+  }
+
+  getRunning(Context ctx) {
+    // TODO
   }
 }
