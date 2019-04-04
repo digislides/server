81a5d92e5000e82ab2cc1ea1d6ebd5e4e6b50f98
tejainece <tejainece@gmail.com>
2019-04-04 16:06:59 +0200

    Monitor api

--- lib/api/monitor_api.dart
+++ lib/api/monitor_api.dart
@@ -2,6 +2,107 @@ part of 'api.dart';
 
 @GenController(path: '/api/monitor')
 class MonitorRoutes extends Controller {
+  /// Route to create a new player
+  @PostJson()
+  Future<Monitor> create(Db db, MonitorCreator data, ServerUser user) async {
+    final accessor = MonitorAccessor(db);
+
+    // Create the channel
+    String id = await accessor.create(data, user.id);
+
+    // Fetch the channel
+    return accessor.get(id);
+  }
+
+  @PostJson(path: '/:id/duplicate')
+  Future<Monitor> duplicate(
+      Context ctx, Db db, String id, ServerUser user) async {
+    final accessor = MonitorAccessor(db);
+
+    // Check if the user has read access
+    Monitor info = await accessor.get(id);
+    if (info == null) {
+      ctx.response = Response(resourceNotFound, statusCode: 401);
+      return null;
+    }
+    if (!info.hasReadAccess(user.id)) {
+      ctx.response = Response(noReadAccess, statusCode: 401);
+      return null;
+    }
+
+    String newId = await accessor.duplicate(info);
+
+    return accessor.get(newId);
+  }
+
+  @PutJson(path: '/:id')
+  Future<Monitor> save(Context ctx, String id, Db db, ServerUser user,
+      MonitorCreator data) async {
+    final accessor = MonitorAccessor(db);
+
+    // Check if the user has read access
+    Monitor info = await accessor.get(id);
+    if (info == null) {
+      ctx.response = Response(resourceNotFound, statusCode: 401);
+      return null;
+    }
+    if (!info.hasWriteAccess(user.id)) {
+      ctx.response = Response(noReadAccess, statusCode: 401);
+      return null;
+    }
+
+    await accessor.save(id, data);
+
+    return accessor.get(id);
+  }
+
+  /// Route to delete a program by id
+  @DeleteJson(path: '/:id')
+  Future<void> delete(Context ctx, String id, Db db, ServerUser user) async {
+    final accessor = MonitorAccessor(db);
+
+    // Check if user has access to the channel
+    Monitor ch = await accessor.get(id);
+    if (ch == null) {
+      ctx.response = Response(resourceNotFound, statusCode: 401);
+      return null;
+    }
+    if (!ch.hasWriteAccess(user.id)) {
+      ctx.response = Response(noWriteAccess, statusCode: 401);
+      return null;
+    }
+
+    // Delete the channel
+    await accessor.delete(id);
+  }
+
+  @GetJson(path: '/:id')
+  Future<Monitor> get(Context ctx, String id, Db db, ServerUser user) async {
+    final accessor = MonitorAccessor(db);
+
+    // Check if the user has read access
+    Monitor info = await accessor.get(id);
+    if (info == null) {
+      ctx.response = Response(resourceNotFound, statusCode: 401);
+      return null;
+    }
+    if (!info.hasReadAccess(user.id)) {
+      ctx.response = Response(noReadAccess, statusCode: 401);
+      return null;
+    }
+
+    return info;
+  }
+
+  @GetJson()
+  Future<List<Monitor>> getAll(Context ctx, Db db, ServerUser user) {
+    final accessor = MonitorAccessor(db);
+
+    // TODO implement pagination
+
+    return accessor.getByUser(user.id, search: ctx.query['search']);
+  }
+
   @WsStream(path: '/:id/rt')
   Future<dynamic> ws(Context ctx, WebSocket ws) async {
     String id = ctx.pathParams['id'];
